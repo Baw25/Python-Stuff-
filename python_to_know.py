32d68519c1393486304e22a042437a08899c5832
Blake Wills <blake.wills525@gmail.com>
2016-12-13 07:39:34 -0800

    Other things to know Python

--- python_to_know.py
+++ python_to_know.py
@@ -148,11 +148,88 @@ if __name__ == '__main__':
 # Nose 
 
 
+#############################################
+# Difference between a dictionary and set: 
+#############################################
+
+#Sets
+
+# Sets are like lists but hold distinct elements 
+# the set doesn't change when you add element X to the 
+# set because X is already there
+
+# In a list it wil change
+
+#Dictionaries 
+
+# are used if you want to make associative data hashable 
+# and super fast lookup times with a constant BigO 
+# No ordering in dictionaries 
+
+
+#############################################
+# Copies and Lists: 
+#############################################
+
+list1 = [1,2,3,4,5]  
+list2 = list1  
+list2.append(6)  
+list2  
+[1, 2, 3, 4, 5, 6]  
+list1  
+[1, 2, 3, 4, 5, 6]  
+
+# making one list equal to another means that you making 
+# two vars pointing to the same list in memory 
+
+
+# Changing dictionary within a dictionary will result 
+# in updates to the outer dictionary too 
+# The solution to this is using a deep copy method 
 
 
 
+#############################################
+# Generators:
+#############################################
+
+# Generators in Python provide us with the advantageous concept of lazy evaluation, 
+# so when we “construct” generators, we don’t actually evaluate some value within it 
+# unless it’s absolutely needed. 
+
+# This saves memory
+
+# Therefore it's better to use xrange() over range() because 
+# xrange() is a generator and its capable of lazy evaluation 
+#range possesses xrange() qualities in python 3 
 
 
+# However, we still need lists because values in generator objects 
+# don't exist unless needed on demand. Also the second loop for gnerators 
+# tends to fail. Therefore, generators are not best for looping. 
+
+# What are generators best for? 
+
+# If we construct a generator that consists of the numbers 1 through N for 
+# large N, and our code ends up only needing the first three numbers, then Python 
+# doesn’t need to construct and store the remaining numbers in memory as it would 
+# for a list. (This hypothetical scenario could happen because there might be a 
+# function with different N values for each call that uses a generator.
+
+#############################################
+# #File Management:
+#############################################
+
+#ex) 
+
+f = open('test.txt', 'r')  
+for line in f:  
+  # Read each line  
+f.close()  
+
+# We use f.close() to free up memory or use with 
+
+# You use File IO for reading and writing files 
 
 
 
